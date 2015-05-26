4459f221b66275bd4995856bbbac4a337b646c88
U Bill Lee <u.bill.lee@f-secure.com>
2015-05-26 11:16:21 +0800

    First commit: Pelican blog generator files and bootstrap3 theme.

---
+++ pelicanconf.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Ubill'
+SITENAME = u"Ubill's Blog"
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Kuala_Lumpur'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+            ('Pelican', 'http://getpelican.com/'),
+            ('Python.org', 'http://python.org/'),
+            ('Jinja2', 'http://jinja.pocoo.org/')
+        )
+
+# Social widget
+SOCIAL = (
+            ('The Original Intro Post', 'http://ubill.github.io/intro-post.html'),
+            ('Facebook', 'https://www.facebook.com'),
+         )
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+#DELETE_OUTPUT_DIRECTORY = True
